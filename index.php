ed8b4dc4ad49bd46f30faa0268a92db50b3e7b51
Aekkasak_Makhamjan <eqadzc@gmail.com>
2023-01-09 12:04:28 +0700

    commit3

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
 <body>
 
     <form action="show.php">
-        what is you name:<input type="text" name="name">
-        what is yot age:<input type="text" name="age">
-        <input type="submit" value="Submit">
+        what is you name:<input type="text" name="name">.<br>
+        what is yot age:<input type="text" name="age">.<br>
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="other">Other<br>
+        <input type="submit" value="Submit"><br>
 
     </form>
 
