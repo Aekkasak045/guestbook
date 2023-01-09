06b25af8dadb994ad44510b1fb157bb4ec7d9961
Aekkasak_Makhamjan <eqadzc@gmail.com>
2023-01-09 11:57:32 +0700

    commit2

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form action="show.php">
+        what is you name:<input type="text" name="name">
+        what is yot age:<input type="text" name="age">
+        <input type="submit" value="Submit">
+
+    </form>
+
+</body>
+
+</html>
